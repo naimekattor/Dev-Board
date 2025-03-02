e7d300e301760c306ef1a965004ddfbc545b28fc
Naimur Rahman Naim <naimekattor@gmail.com>
2025-03-02 19:33:25 +0600

    add some

--- custom.js
+++ custom.js
@@ -7,15 +7,22 @@ event.target.style.backgroundColor="#3752FD";
 event.target.style.opacity="30%";
 document.getElementById("total-completion").innerText=Number(document.getElementById("total-completion").innerText)+1;
 let historyList=document.getElementById("history-list");
-let history=document.createElement("p");
+const history=document.createElement("p");
 history.style.backgroundColor="#F4F7FF";
 history.style.margin="10px";
 history.style.padding="10px";
 const card=event.target.closest("div");
 const parentCard=card.parentElement;
 const cardHead=parentCard.querySelector("h1").innerText;
-let updateHistory=history.innerText=`You have Complete The Task ${cardHead} at ${new Date().toLocaleTimeString()}`;
-historyList.appendChild(updateHistory);
+history.innerText=`You have Complete The Task ${cardHead} at ${new Date().toLocaleTimeString()}`;
+console.log(history.innerText);
+
+// if (historyList) {
+//   historyList.appendChild(history.innerText);
+// }else{
+//   console.error("History List Not Found")
+// }
+historyList.appendChild(history);
 
 
 if (Number(taskAssign)===1) {
